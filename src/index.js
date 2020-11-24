0618f0ff5c450bdabade3a32403de56d9d84bcfe
Safvan <safvanparangodath12@gmail.com>
2020-11-24 23:39:14 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React ,{Component}from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+let skiData={
+  total:60,
+  powder:20,
+  background:55,
+  goal:100
+}
+
+const getPercent = (decimal) => {
+  return  decimal * 100+"%"
+}
+
+const goalProcess = (total,goal) => {
+  return getPercent(total/goal)
+}
+
+const SkiDayCounter = ({total,powder,background,goal}) => {
+  return(
+    <section>
+        <div>
+          <p>Total Days :{total}</p>
+        </div>
+        <div>
+          <p>Powder :{powder}</p>
+        </div>
+        <div>
+          <p>background :{background}</p>
+        </div>
+        <div>
+          <p>goal percet:{goalProcess(total,goal)}</p>
+        </div>
+
+      </section>
+  )
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<SkiDayCounter 
+  total={skiData.total}
+  powder={skiData.powder}
+  background={skiData.background}
+  goal={skiData.goal}
+/>,
+document.getElementById('root')
+)
